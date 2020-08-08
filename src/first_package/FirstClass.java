6f25719a8abac9b0f3ae5b8c907aca4c5186e8c3
Venus27 <aliuzung@gmail.com>
2020-08-08 14:28:46 -0400

    last changes on venus branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,10 +5,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("our first commit from local repo");
 		System.out.println("our second commit from local repository");
-<<<<<<< HEAD
+
 		System.out.println("now I am on my venus branch");
-=======
 		
->>>>>>> feature
+
 	}
 }
